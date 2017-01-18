34d2b447ae8b8fe79b754abb1fb2b50d534f8b1a
2502514088 <2502514088@qq.com>
2017-01-18 16:44:37 +0800

    x

--- Source/IsWhat/IsWhatGameModeBase.cpp
+++ Source/IsWhat/IsWhatGameModeBase.cpp
@@ -2,7 +2,8 @@
 
 #include "IsWhat.h"
 #include "IsWhatGameModeBase.h"
-
+#include <iostream>
+using namespace std;
 void AIsWhatGameModeBase::StartPlay()
 {
 	Super::StartPlay();
@@ -12,6 +13,9 @@ void AIsWhatGameModeBase::StartPlay()
 		// 显示调试信息五秒。 
 		// -1“键”值（首个参数）说明我们无需更新或刷新此消息。
 		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Hello World, this is FPSGameMode!"));
+		std::cout << "???????"<<std::endl;
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Hi~it's a game?oh,i like is!"));
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Wo,you look the game,Do you batter good ider for game?push my 2502514088@qq.com,please.Thank you!"));
 	}
 }
 
